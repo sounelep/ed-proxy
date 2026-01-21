73673dfca34f35dc4cf28b1e381be9b5d2fdc60a
sounelep <soune.lep@gmail.com>
2026-01-21 07:05:54 +0100

    Create cors.js

---
+++ api/cors.js
@@ -0,0 +1,12 @@
+export default function cors(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return true;
+  }
+
+  return false;
+}

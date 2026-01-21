273468ba67cc034e485d23064aa864854b8ae14f
sounelep <soune.lep@gmail.com>
2026-01-21 06:46:14 +0100

    Update ed-cdt.js

--- api/ed-cdt.js
+++ api/ed-cdt.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Méthode non autorisée" });
   }
